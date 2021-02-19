336b9deab0bbb9d1840c92bd1cfc2eda3322487b
KMurphy-5 <kevincitorios@gmail.com>
2021-02-19 12:04:17 -0800

    adding Imenu interface and PartsMenu class

---
+++ PartsStore/StoreApp/StoreUI/PartsMenu.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace StoreUI
+{
+    public class PartsMenu : IMenu
+    {
+        public void Start()
+        {
+           Console.WriteLine("This is the Main Menu");
+        }
+    }
+}
